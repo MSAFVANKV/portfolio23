b1b6d0d16e9a0b5c09b299a6316a31fa39c3ec1c
Muhammed Safvan KV <mskvphed@gmail.com>
2023-10-03 18:05:13 +0530

    deploy vite-2

--- portfolio23/vite.config.js
+++ portfolio23/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Poertfolio-2023/"
+  base: "/portfolio23/"
 })
